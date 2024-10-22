63df1ff511fe2182db63a01b85af47feddbfea33
Winlypse <nts3@o2.pl>
2024-10-22 22:50:06 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noBreakingProjectles/NoBreakingProjectles.kt
@@ -0,0 +1,14 @@
+package winlyps.noBreakingProjectles
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoBreakingProjectles : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
